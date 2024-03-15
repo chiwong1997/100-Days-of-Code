54e7aa578e1e8ca058cfcbde2cf04120b9c2e54e
chiyinwong <chiyinwong97@gmail.com>
2024-03-16 10:29:29 +1100

    Day 1 Complete
    Finished Day 1 - band name generator

---
+++ Beginner/Day-1/band-name-generator.py
@@ -0,0 +1,11 @@
+"""
+Project: Band Name Generator 
+Concepts: printing, commenting, debugging, string manipulation, variables 
+
+"""
+
+print("Welcome to the Band Name Generator")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+band_name = city + " " + pet
+print("Your new band name can be " + band_name)
---
+++ Beginner/Day-1/day-1-practice.py
@@ -0,0 +1,25 @@
+### Printing
+print("Hello World")
+print("Hell to the yeah ---- !!! sheeeesh")
+
+# single and double quotes and escape values
+print("A single quote 'inside' a double quote")
+print('A double quote "inside" a double quote')
+print("An escape \"Quote\" look at this")
+
+# new line command \n
+print("Hello World!\nHello World!")
+
+# concatenate strings 
+print("Hello" + "  Chi")
+
+### Input Function 
+# input("What is your name?")
+
+# print("Hello " + input("What is your name?"))
+
+# save your input as a variable 
+#name = input("What is your name?")
+#print(name)
+
+print(len("hello world"))
