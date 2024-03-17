af5ad46eb6726c554ae34afc1544ce6a1182fddd
chiyinwong <chiyinwong97@gmail.com>
2024-03-17 23:02:53 +1100

    up to day 4 project

--- Beginner/Day-4/day-4-practice.py
+++ Beginner/Day-4/day-4-practice.py
@@ -5,3 +5,11 @@ print(random.randint(100,200))
 
 love_score = random.randint(1,100)
 print(f"Your love score is {love_score}")
+
+# Data Structures 
+# Lists 
+
+fruits = ["cherry", "blueberry", "strawberry", "apple"]
+print(fruits)
+
+states_of_america = ["Delaware", "Pennsylvania", "Virginia"]
