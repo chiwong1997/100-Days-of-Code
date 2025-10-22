6d93f4a5e2954f7b3bc2b9c4f840a4176e7ca415
chiwong1997 <163490644+chiwong1997@users.noreply.github.com>
2025-10-22 11:30:30 +0800

    Create day-26.py
    started day 26

---
+++ Intermediate/day-26.py
@@ -0,0 +1,8 @@
+# List Comprehension
+# new_list = [new_item for item in list]
+
+numbers = [1,2,3,4,5]
+add_one = [number + 1 for number in numbers]
+squared_numbers = [number ** 2 for number in numbers]
+print(add_one)
+print(squared_numbers)
