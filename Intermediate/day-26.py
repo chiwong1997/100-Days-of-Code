b4d6bd82957c14e0cba63a3694badcbbec8e5cd1
chiwong1997 <97cywong@gmail.com>
2025-11-27 16:13:37 +0800

    finished video 1
    video 1

--- Intermediate/day-26.py
+++ Intermediate/day-26.py
@@ -6,3 +6,17 @@ add_one = [number + 1 for number in numbers]
 squared_numbers = [number ** 2 for number in numbers]
 print(add_one)
 print(squared_numbers)
+
+name = 'Chi'
+new_list = [letter for letter in name]
+print(new_list)
+
+new = [number * 2 for number in range(1,5)]
+print(new)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [n for n in names if len(n) <= 4]
+print(short_names)
+
+long_names = [n.upper() for n in names if len(n)>4]
+print(long_names)
