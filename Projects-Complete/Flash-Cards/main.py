93c60da7f831a52a6162d6b8bb6a03166efeb5b9
chiyinwong <chiyinwong97@gmail.com>
2026-01-05 18:03:38 +0800

    Completed part 2

--- Projects-Complete/Flash-Cards/main.py
+++ Projects-Complete/Flash-Cards/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from pathlib import Path
+import pandas as pd
+import random
 
 # File Opening
 BASE_DIR = Path(__file__).parent
@@ -13,6 +15,17 @@ wrong_image_path = BASE_DIR / "images/wrong.png"
 BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("Ariel", 32, "italic")
 WORD_FONT = ("Ariel", 60, "bold")
+LANGUAGE = "French"
+ENGLISH = "English"
+
+# Functions
+def change_word():
+    index = random.randint(0, len(df) - 1)
+    card_front_canvas.itemconfig(word_text, text=df[index][LANGUAGE])
+    card_front_canvas.itemconfig(title_text, text = LANGUAGE)
+
+# ------------------- Data Loading ------------------
+df = pd.read_csv(data_file_path).to_dict(orient="records") # converts to a list of dictionaries
 
 # ------------------ UI Setup ------------------
 window = Tk()
@@ -24,18 +37,20 @@ card_front_image = PhotoImage(file=card_front_image_path)
 card_front_canvas = Canvas(width=800, height=526)
 card_front_canvas.create_image(400, 263, image=card_front_image)
 card_front_canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-card_front_canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
-card_front_canvas.create_text(400, 263, text="Word", font=WORD_FONT)
+title_text = card_front_canvas.create_text(400, 150, text="", font=TITLE_FONT)
+word_text = card_front_canvas.create_text(400, 263, text="", font=WORD_FONT)
 card_front_canvas.grid(column=0, row=0, columnspan=2)
 
 # Wrong Button
 wrong_image = PhotoImage(file=wrong_image_path)
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=change_word)
 wrong_button.grid(column=0, row=1)
 
 # Right Button
 right_image = PhotoImage(file=right_image_path)
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command = change_word)
 right_button.grid(column=1, row=1)
+
+change_word()
 
 window.mainloop()
